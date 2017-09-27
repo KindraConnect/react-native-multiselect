22dd09ea6282fdfb2cb76274fae1376dfa5eb885
drewmca <drew@unitdev.com>
2017-09-27 12:11:33 -0700

    PropTypes fix

--- MultiSelect.js
+++ MultiSelect.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ListView, FlatList, SectionList } from 'react-native';
 import MultiSelectRow from './MultiSelectRow';
 
